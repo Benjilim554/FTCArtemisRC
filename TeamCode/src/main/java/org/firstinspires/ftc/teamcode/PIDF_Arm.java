821387c36b1948f398a4e3fe29c5a46516239b34
Benjilim554 <benjilim2009@gmail.com>
2025-03-24 00:43:24 -0700

    Update PIDF_Arm.java
    -added feed forward (might not work)
    -added telemetry for FTCDashboard
    -DO NOT CHANGE P, I, D! you're working with the constants kP, kI, and kD in FTCDashboard.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PIDF_Arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PIDF_Arm.java
@@ -1,47 +1,70 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+@Config
+@TeleOp
 public abstract class PIDF_Arm extends LinearOpMode {
+    public static double f = 0;
 
-    public void armToposition(DcMotor arm, int target, double kp, double ki, double kd, OpMode opmode) {
+    private final double ticks_in_degree = 700 / 180.0;
+
+    private DcMotorEx armMotor;
+
+    public void armToposition(DcMotor armMotor, int target, double kp, double ki, double kd, OpMode opmode) {
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+
+        armMotor = hardwareMap.get(DcMotorEx.class, "armMotor");
         ElapsedTime timer = new ElapsedTime();
+
         int MDE = 3;
+
         double previousTime = 0, previousError = 0;
         double p = 0, i = 0, d = 0; // where p = proportional, i = integral, d = derivative
         double max_i = 0.2, min_i = -0.2;
         double power;
 
-        while(Math.abs(target - arm.getCurrentPosition()) > 3 && ((LinearOpMode)opmode).opModeIsActive()) {
+        while(Math.abs(target - armMotor.getCurrentPosition()) > 3 && ((LinearOpMode)opmode).opModeIsActive()) {
+            double ff = Math.cos(Math.toRadians(target / ticks_in_degree) * f);
             double currentTime = timer.milliseconds();
-            double error = target - arm.getCurrentPosition();
+            double error = target - armMotor.getCurrentPosition();
 
             // Account for the Proportional Error
             p = kp * error; // directly proportional to the error
 
             // Account for the Integral Error (should we account for the integral error? P & D is useful beside I, because I is just a pain in the butt for tuning)
-         /*   i = ki * (error + (currentTime - i));
+            i = ki * (error + (currentTime - i));
 
             if (i > max_i) {
                 i = max_i;
             } else if (i < min_i) {
                 i = min_i;
-            } // bounding with the min and max */
+            } // bounding with the min and max
 
             // Derivative Error
             d = kd *  (error - previousError) / (currentTime - previousTime); // directly proportional to the rate of change of error)
 
             power = p + i + d; // remove i if you're not using integral error
-            arm.setPower(power);
+            armMotor.setPower(power + ff);
 
             // Save Values
             previousError = error;
             previousTime = currentTime;
+
+            telemetry.addData("pos: ", armMotor.getCurrentPosition());
+            telemetry.addData("target: ", target);
+            telemetry.addData("error: ", error);
+            telemetry.addData("target: ", target);
+            telemetry.update();
         }
      //   arm.setPower(p + i + d);
     }
-    //TODO: Make a FeedForward to compensate for the gravity of the arm, (7477vidyt)
 }
